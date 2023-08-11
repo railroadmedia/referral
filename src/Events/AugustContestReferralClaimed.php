e87be4860bbe5f6a4c4a299901922432ea58f90b
Alexandre Severo <alexandre@musora.com>
2023-08-11 11:19:23 -0700

    Add event for august referral contest

---
+++ src/Events/AugustContestReferralClaimed.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Railroad\Referral\Events;
+
+use Railroad\Referral\Models\Referrer;
+
+class AugustContestReferralClaimed
+{
+    /**
+     * ReferralClaimed constructor.
+     */
+    public function __construct(Referrer $referrer, int $productId, int $userId)
+    {
+        $this->referrer = $referrer;
+        $this->productId = $productId;
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return Referrer
+     */
+    public function getReferrer(): Referrer
+    {
+        return $this->referrer;
+    }
+
+    /**
+     * @param Referrer $referrer
+     */
+    public function setReferrer(Referrer $referrer): void
+    {
+        $this->referrer = $referrer;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId(): int
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param int $productId
+     */
+    public function setProductId(int $productId): void
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+}
